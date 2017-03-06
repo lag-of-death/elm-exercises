aa309fe84684b32177c9bec398d3e32f6be58ce5
maggot <mateuszwit21@gmail.com>
2017-03-06 07:24:18 +0100

    Add counter example

---
+++ TEA/Counter/From.elm
@@ -0,0 +1,8 @@
+module From exposing (..)
+
+import Html
+
+
+main : Html.Html String
+main =
+    Html.text "Hello World!"
---
+++ TEA/Counter/To.elm
@@ -0,0 +1,59 @@
+module To exposing (..)
+
+import Html exposing (..)
+import Html.App as App
+import Html.Events exposing (..)
+
+
+main : Program Never
+main =
+    App.beginnerProgram
+        { model = initialModel
+        , update = update
+        , view = view
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    Int
+
+
+initialModel : Model
+initialModel =
+    0
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Add
+    | Sub
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Add ->
+            model + 1
+
+        Sub ->
+            model - 1
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick Add ] [ text "+" ]
+        , text <| toString model
+        , button [ onClick Sub ] [ text "-" ]
+        ]
